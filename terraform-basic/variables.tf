81a3ca2551f0747da326fbea9f397207394591ec
Javier Cuenca Perez <javiercuencaperez@gmail.com>
2026-05-23 18:33:22 +0200

    Update variables.tf

--- terraform-basic/variables.tf
+++ terraform-basic/variables.tf
@@ -1,13 +1,19 @@
+# EN: Server hostname variable
+# ES: Variable del nombre del servidor
 variable "server_name" {
   type    = string
   default = "srv-devops-junior"
 }
 
+# EN: Environment name variable
+# ES: Variable del entorno
 variable "environment" {
   type    = string
   default = "lab"
 }
 
+# EN: Server IP address variable
+# ES: Variable de dirección IP del servidor
 variable "server_ip" {
   type    = string
   default = "192.168.56.50"
