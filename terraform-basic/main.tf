ad45e08c0e25a56c4355218eb7cfd2145ac5b67a
Javier Cuenca Perez <javiercuencaperez@gmail.com>
2026-05-23 18:32:39 +0200

    Update main.tf

--- terraform-basic/main.tf
+++ terraform-basic/main.tf
@@ -1,8 +1,14 @@
 terraform {
+
+  # EN: Minimum required Terraform version
+  # ES: Versión mínima requerida de Terraform
   required_version = ">= 1.0"
 }
 
+# EN: Create local file with server information
+# ES: Crear archivo local con información del servidor
 resource "local_file" "server_info" {
+
   filename = "server-info.txt"
 
   content = <<EOT
